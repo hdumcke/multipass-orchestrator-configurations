b4d354ac83e47fe56111cffe606deb5d7e97f30d
Horst Dumcke <hdumcke@marlysys.com>
2023-01-09 11:40:10 +0100

    pinned esp-idf to release/v5.0

--- esp-idf/build.sh
+++ esp-idf/build.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt-get install -y git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 mkdir -p ~/esp
 cd ~/esp
-git clone --recursive https://github.com/espressif/esp-idf.git
+git clone -b release/v5.0 --recursive https://github.com/espressif/esp-idf.git
 cd ~/esp/esp-idf
 ./install.sh esp32s3
 ./install.sh all
