7b0a93b0ef97ddd086ca9afb4bd1c0fc7dc9a9d5
Horst Dumcke <hdumcke@marlysys.com>
2023-01-28 23:40:04 +0100

    working on armtoolchain

--- armtoolchain/build.sh
+++ armtoolchain/build.sh
@@ -3,7 +3,7 @@
 sudo apt install -y make
 wget -O  ~/arm-none-eabi.tar.xz https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz?rev=95edb5e17b9d43f28c74ce824f9c6f10&hash=176C4D884DBABB657ADC2AC886C8C095409547C4
 cd ~
-/usr/bin/tar -xf ~/arm-none-eabi.tar.xz -O arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi
+/usr/bin/tar -xf ~/arm-none-eabi.tar.xz
 cd ~/linorobot2_hoverboard/GD32-hover-master-slave/
 make GCC_PATH=~/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin
 mkdir ~/slave
