08df776a8f52dc19bc296cca9fe97de8eecb0aa9
Horst Dumcke <hdumcke@marlysys.com>
2023-01-28 19:01:30 +0100

    working on armctoolchain

--- armctoolchain/compile.sh
+++ armctoolchain/compile.sh
@@ -3,3 +3,9 @@
 /usr/bin/tar -xf ~/arm-none-eabi.tar.xz
 cd ~/linorobot2_hoverboard/GD32-hover-master-slave/
 make GCC_PATH=~/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin
+mkdir ~/slave
+cp build/firmware.bin ~/slave/
+sed -i "s/\/\/#define MASTER/#define MASTER/" Inc/config.h
+sed -i "s/#define SLAVE/\/\/#define SLAVE/" Inc/config.h
+mkdir ~/master
+cp build/firmware.bin ~/master/
