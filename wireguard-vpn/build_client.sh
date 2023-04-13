ee688fc073d01db665f91c1e5cf6930c8da6e429
Horst Dumcke <hdumcke@marlysys.com>
2023-04-13 09:13:28 +0200

    WIP

--- wireguard-vpn/build_client.sh
+++ wireguard-vpn/build_client.sh
@@ -3,5 +3,32 @@
 sudo apt update
 sudo apt install -y wireguard
 
+wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireguard/publickey
+
 # allow ipv4 forwarding
 sudo sed -i "s/#net.ipv4.ip_forward/net.ipv4.ip_forward/" /etc/sysctl.conf
+
+cat > /tmp/wg0.conf << EOF
+[Interface]
+PrivateKey = $(sudo cat /etc/wireguard/privatekey)
+Address = 10.0.0.2/24
+
+[Peer]
+PublicKey = $SERVERKEY
+AllowedIPs = 10.0.0.0/24
+Endpoint = $SERVERIP.1:51820
+EOF
+
+sudo cp /tmp/wg0.conf /etc/wireguard/
+
+sudo chmod 600 /etc/wireguard/{privatekey,wg0.conf}
+
+cat > ~/connect.sh << EOF
+#!/bin/bash
+
+export SERVERKEY=
+export SERVERIP=
+sudo wg-quick up wg0
+EOF
+
+chmod +x ~/connect.sh
--- wireguard-vpn/build_server.sh
+++ wireguard-vpn/build_server.sh
@@ -8,6 +8,27 @@ wg genkey | sudo tee /etc/wireguard/privatekey | wg pubkey | sudo tee /etc/wireg
 # allow ipv4 forwarding
 sudo sed -i "s/#net.ipv4.ip_forward/net.ipv4.ip_forward/" /etc/sysctl.conf
 
-sudo cp multipass-orchestrator-configurations/wireguard-vpn/wg0.conf /etc/wireguard/
+cat > /tmp/wg0.conf << EOF
+[Interface]
+Address = 10.0.0.1/24
+SaveConfig = true
+ListenPort = 51820
+PrivateKey = $(sudo cat /etc/wireguard/privatekey)
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o enp0s2 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o enp0s2 -j MASQUERADE
+EOF
+
+sudo cp /tmp/wg0.conf /etc/wireguard/
 
 sudo chmod 600 /etc/wireguard/{privatekey,wg0.conf}
+
+cat > ~/add-peer.sh << EOF
+#!/bin/bash
+PEERPUBKEY=
+sudo wg set wg0 peer PEERPUBKEY allowed-ips 10.0.0.2,10.0.0.100
+EOF
+
+chmod +x ~/add-peer.sh
+
+sudo systemctl enable wg-quick@wg0.service
+sudo systemctl start wg-quick@wg0.service
