d841f33351b3d030ec71caebaa6f3831d0eba8b5
Horst Dumcke <hdumcke@marlysys.com>
2023-05-04 21:32:17 +0200

    testing build123d

--- build123d/build.sh
+++ build123d/build.sh
@@ -14,15 +14,17 @@ python3 -m pip install -e .
 cd ~
 git clone https://github.com/bernhard-42/three-cad-viewer.git
 
+exit 0
+
 cd ~
 curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.shA
 sudo bash nodesource_setup.sh
+sudo apt update
 sudo apt-get install -y nodejs
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y yarn
 sudo apt install -y --no-install-recommends yarn
 
 cd ~/three-cad-viewer
