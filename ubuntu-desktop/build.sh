ad21037d15d8c45aa891af3a6f76907958caf4e2
Horst Dumcke <hdumcke@marlysys.com>
2023-06-10 11:44:56 +0200

    more THP disabled

--- ubuntu-desktop/build.sh
+++ ubuntu-desktop/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo never | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+echo never | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
 
 sudo apt update
 sudo apt install -y ubuntu-desktop xrdp
