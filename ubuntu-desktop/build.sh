62ffac08dbdb3475efa1ed6c818533df40090da2
Horst Dumcke <hdumcke@marlysys.com>
2023-06-10 12:25:03 +0200

    removed THP stuff

--- ubuntu-desktop/build.sh
+++ ubuntu-desktop/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo never | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
-echo never | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
-
 sudo apt update
 sudo apt install -y ubuntu-desktop xrdp
 echo ubuntu:ubuntu | sudo chpasswd
