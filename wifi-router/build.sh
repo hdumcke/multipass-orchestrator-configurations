6fa5f6c87a96dabd7b9d3418b91ae616a7aaecb1
Horst Dumcke <hdumcke@marlysys.com>
2023-10-13 11:13:52 +0200

    updated wifi-router

--- wifi-router/build.sh
+++ wifi-router/build.sh
@@ -3,6 +3,11 @@
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt install -y dnsmasq
+sudo apt install -y dnsmasq iptables-persistent
 
 cat ~/multipass-orchestrator-configurations/lab-env/keys.txt >>  ~/.ssh/authorized_keys
+sudo cp ~/multipass-orchestrator-configurations/wifi-router/dnsmasq.conf /etc
+sudo cp ~/multipass-orchestrator-configurations/wifi-router/iptables.rules /etc/iptables/rules.v4
+
+sudo sed -i "s/#DNSStubListener=yes/DNSStubListener=no/" /etc/systemd/resolved.conf
+sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
