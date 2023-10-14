8477c644dc82df451ca16d30795905c25950f19c
Horst Dumcke <hdumcke@marlysys.com>
2023-10-14 10:57:39 +0200

    updated TeamCdR2024

--- TeamCdR2024/build.sh
+++ TeamCdR2024/build.sh
@@ -3,10 +3,18 @@
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt -y install python3-venv build-essential gdb clang-format
+sudo apt -y install python3-venv build-essential gdb clang-format cmake python3-dev nginx
 
-python -mvenv dev
+
+python3 -mvenv dev
 source dev/bin/activate
 pip install coverage
+pip install gcovr
 
 cat ~/multipass-orchestrator-configurations/TeamCdR2024/keys.txt >>  ~/.ssh/authorized_keys
+
+# Highly insecure, use only for local VM
+sudo sed -i "s|/var/www/html|/home/ubuntu|" /etc/nginx/sites-enabled/default
+sudo sed -i "/404\./a autoindex on;" /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx
+sudo chown -R ubuntu.www-data /home/ubuntu
