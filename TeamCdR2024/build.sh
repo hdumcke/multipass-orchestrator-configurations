02f7201fa19b3fd5aa4a880b1c3d92ba5f39b16b
Horst Dumcke <hdumcke@marlysys.com>
2023-10-28 15:55:22 +0200

    updated build script for TeamCdR2024

--- TeamCdR2024/build.sh
+++ TeamCdR2024/build.sh
@@ -3,8 +3,7 @@
 sudo apt update
 sudo apt -y upgrade
 
-sudo apt -y install python3-venv build-essential gdb clang-format cmake python3-dev nginx
-
+sudo apt -y install python3-venv build-essential gdb clang-format cmake python3-dev nginx libirrlicht1.8 libirrlicht-dev
 
 python3 -mvenv dev
 source dev/bin/activate
@@ -12,6 +11,11 @@ pip install coverage
 pip install gcovr
 
 cat ~/multipass-orchestrator-configurations/TeamCdR2024/keys.txt >>  ~/.ssh/authorized_keys
+
+wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb
+sudo apt-install -y -f ./gcm-linux_amd64.2.3.2.deb
+git-credential-manager configure
+git config --global credential.credentialStore plaintext
 
 # Highly insecure, use only for local VM
 sudo sed -i "s|/var/www/html|/home/ubuntu|" /etc/nginx/sites-enabled/default
